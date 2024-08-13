d973d0430eee68503dd8d81dcce9680231cd0b7f
seva <vsevolod.synytskyi@gmail.com>
2024-08-13 13:05:27 +0300

    aceternity setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Seva",
   description: "Seva's personal website",
+  icons: {
+    icon: "./favicon.png",
+  },
 };
 
 export default function RootLayout({
