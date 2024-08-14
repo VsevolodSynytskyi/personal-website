e3fe01e8a9859debba6d3c0b6e35a0efa8135ec6
seva <vsevolod.synytskyi@gmail.com>
2024-08-14 10:16:26 +0300

    vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Toaster } from "@/components/ui/sonner";
 import { cn } from "@/lib/utils";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -29,6 +30,7 @@ export default function RootLayout({
       >
         <Toaster closeButton position="top-center" />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
