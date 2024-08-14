cd873753911a672985194bd36e3d5c4fa21f4429
seva <vsevolod.synytskyi@gmail.com>
2024-08-14 16:35:05 +0300

    frontend email form setup

--- components/contacts/MessageBox.tsx
+++ components/contacts/MessageBox.tsx
@@ -1,37 +1,77 @@
-import { Label } from "@radix-ui/react-label";
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Send } from "lucide-react";
-import { useState } from "react";
 import { toast } from "sonner";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
 
+const labelText = `Поле, щоб привітатись.`;
+const messagePlaceholder = `Повідомлення.`;
+
+const formSchema = z.object({
+  message: z
+    .string()
+    .min(1, { message: "Не скупіться на слова." })
+    .max(2000, { message: "Давайте почнем діалог лаконічніше." }),
+});
+
 const MessageBox: React.FC = () => {
-  const [messageText, setMessageText] = useState<string>("");
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      message: "",
+    },
+  });
 
-  const sendMessage = async (message: string) => {};
-
-  const onSendClick = () => {
-    if (!messageText) return;
-    sendMessage(messageText);
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
     toast.success("Повідомлення відправлено", {
-      description: messageText,
+      description: values.message,
     });
-    setMessageText("");
   };
+
   return (
     <div className="flex flex-col gap-2">
-      <Label htmlFor="message">Поле, щоб привітатись.</Label>
-      <Textarea
-        value={messageText}
-        onChange={(e) => {
-          setMessageText(e.target.value);
-        }}
-        placeholder="Type your message here."
-        id="message"
-      />
-      <Button onClick={onSendClick} disabled={!messageText}>
-        <Send className="w-4 h-4 mr-2" /> Відправити
-      </Button>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>{labelText}</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder={messagePlaceholder} {...field} />
+                  </FormControl>
+                  {/* <FormDescription>
+                  lorem ipsum
+                </FormDescription> */}
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <Button
+            type="submit"
+            disabled={!form.formState.isValid}
+            className="w-full"
+          >
+            <Send className="w-4 h-4 mr-2" /> Відправити
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 };
