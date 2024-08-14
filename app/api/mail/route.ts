7f0f1181170dee55ac2df172b2c3701818401e6e
seva <vsevolod.synytskyi@gmail.com>
2024-08-14 17:42:09 +0300

    resend type fix

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -1,4 +1,3 @@
-import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 export async function POST(request: Request) {
@@ -7,24 +6,11 @@ export async function POST(request: Request) {
   const url = new URL(request.url);
   const baseUrl = `${url.protocol}//${url.host}`;
 
-  try {
-    const resend = new Resend(process.env.RESEND_API_KEY);
-    const response = await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: "vsevolod.synytskyi@gmail.com",
-      subject: "Hello World",
-      html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
-    });
-
-    if (response.ok) {
-      return NextResponse.json({ message: "Email sent successfully" });
-    } else {
-      return NextResponse.json(
-        { message: "Failed to send email" },
-        { status: 500 }
-      );
-    }
-  } catch (error) {
-    return NextResponse.json({ message: "An error occurred" }, { status: 500 });
-  }
+  const resend = new Resend(process.env.RESEND_API_KEY);
+  const response = await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: "vsevolod.synytskyi@gmail.com",
+    subject: "Hello World",
+    html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
+  });
 }
