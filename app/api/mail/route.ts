deb2e9fd587ac667c0a3d0fc337a635a74b19617
seva <vsevolod.synytskyi@gmail.com>
2024-08-15 11:34:55 +0300

    mail resend setup

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -1,16 +1,28 @@
 import { Resend } from "resend";
 
-export async function POST(request: Request) {
+export async function POST(request: Request, response: Response) {
   const { message } = await request.json();
 
   const url = new URL(request.url);
   const baseUrl = `${url.protocol}//${url.host}`;
 
   const resend = new Resend(process.env.RESEND_API_KEY);
-  const response = await resend.emails.send({
-    from: "onboarding@resend.dev",
+  const fromEmail = process.env.NO_REPLY_EMAIL;
+
+  if (!fromEmail) {
+    return Response.json({ message: "Sending email is not specified" });
+  }
+
+  const { data, error } = await resend.emails.send({
+    from: fromEmail,
     to: "vsevolod.synytskyi@gmail.com",
-    subject: "Hello World",
-    html: `${message}`,
+    subject: `Message from ${baseUrl}`,
+    html: `<p>${message}</p>`,
   });
+
+  if (error) {
+    return Response.json(error);
+  }
+
+  return Response.json({ message: "Email sent successfully" });
 }
