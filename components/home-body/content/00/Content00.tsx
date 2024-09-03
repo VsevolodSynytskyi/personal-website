91db2a2da846f57572ad3eb43af7d24fb1a1800f
seva <vsevolod.synytskyi@gmail.com>
2024-09-03 21:10:11 +0300

    mdx init

--- components/home-body/content/00/Content00.tsx
+++ components/home-body/content/00/Content00.tsx
@@ -1,53 +1,22 @@
+import { useLocale } from "next-intl";
+import { notFound } from "next/navigation";
 import React from "react";
-import ContentSection from "../../ContentSection";
+import {
+  default as Content00en,
+  default as Content00uk,
+} from "./Content00en.mdx";
 
 const Content00: React.FC = () => {
-  return (
-    <>
-      <ContentSection
-        title="Дизайн і Розробка"
-        contentBlocks={[
-          {
-            content: (
-              <>
-                <p>Я працюю на стику дизайну і розробки.</p>
-                <p>
-                  Мене захоплює проектування інтерфейсів, які викликають
-                  відчуття "сів і поїхав", а розробка - інструмент втілення
-                  творчих задумів.
-                </p>
-                <p>
-                  Основний технічний стек: React JS, Typescript, TailwindCSS,
-                  Framer Motion...
-                </p>
-              </>
-            ),
-          },
-        ]}
-      />
-      <ContentSection
-        title="Останній Досвід"
-        contentBlocks={[
-          {
-            content: (
-              <p>
-                Останні 2+ роки покращував UX сервісу для управління
-                будівельними проектами в Штатах. (SaaS, B2B, construction, USA){" "}
-              </p>
-            ),
-          },
-          {
-            content: (
-              <p>
-                Завжди кортить створювати власні проекти: мав коворкінг,
-                волонтерський сервіс і пару пет-проектів.
-              </p>
-            ),
-          },
-        ]}
-      />
-    </>
-  );
+  const locale = useLocale();
+
+  switch (locale) {
+    case "en":
+      return <Content00en />;
+    case "uk":
+      return <Content00uk />;
+    default:
+      notFound();
+  }
 };
 
 export default Content00;
--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+import createMDX from "@next/mdx";
 import createNextIntlPlugin from "next-intl/plugin";
 /** @type {import('next').NextConfig} */
 
@@ -20,9 +21,16 @@ const nextConfig = {
       },
     ];
   },
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-// next-intl setup
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+// Merge MDX config with Next.js config
 const withNextIntl = createNextIntlPlugin("./lib/i18n/request.ts");
 
-export default withNextIntl(nextConfig);
+// next-intl setup + MDX setup
+export default withMDX(withNextIntl(nextConfig));
