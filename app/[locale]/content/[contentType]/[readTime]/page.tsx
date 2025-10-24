8eaba2deb2de99197173a10f23397a3e98da59b6
seva <vsevolod.synytskyi@gmail.com>
2025-10-24 12:59:32 +0300

    generate static params for content pages, unify locale definitions, and fix MDX formatting inconsistencies

--- app/[locale]/content/[contentType]/[readTime]/page.tsx
+++ app/[locale]/content/[contentType]/[readTime]/page.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import ContentPageBody from "@/components/home-body/ContentPageBody";
 import { PageParamContent, PageParamLocale } from "@/lib/customTypes";
 import contentPageParamsToValues from "@/lib/content-slider/contentPageParamsToValues";
+import { locales } from "@/lib/i18n/locales";
 
 interface ContentBodyPageProps {
   params: Promise<PageParamLocale & PageParamContent>;
@@ -15,5 +16,23 @@ const ContentBodyPage: NextPage<ContentBodyPageProps> = async ({ params }) => {
   });
   return <ContentPageBody {...contentValues} />;
 };
+
+export async function generateStaticParams() {
+  const params = [];
+
+  for (const locale of locales) {
+    for (let contentTypeIndex = 0; contentTypeIndex < 4; contentTypeIndex++) {
+      for (let readTimeIndex = 0; readTimeIndex < 4; readTimeIndex++) {
+        params.push({
+          locale,
+          contentType: contentTypeIndex,
+          readTime: readTimeIndex,
+        });
+      }
+    }
+  }
+
+  return params;
+}
 
 export default ContentBodyPage;
