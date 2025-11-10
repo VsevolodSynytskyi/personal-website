6ec551bb7b73a8f9ca6f8eb0c715f552b03e6686
seva <vsevolod.synytskyi@gmail.com>
2025-11-10 13:12:15 +0200

    uk content Updated

--- components/home-body/ContentPageBody.tsx
+++ components/home-body/ContentPageBody.tsx
@@ -4,80 +4,93 @@ import { ContentMatrix, ContentParamValues } from "@/lib/customTypes";
 import { isValidLocale } from "@/lib/i18n/locales";
 import { AnimatePresence, motion } from "framer-motion";
 import { useLocale } from "next-intl";
-import Content00en from "./content/00/Content00en";
-import Content00uk from "./content/00/Content00uk.mdx";
 
-import Content01en from "./content/01/Content01en";
-import Content01uk from "./content/01/Content01uk.mdx";
+import Professional1Uk from "./content/proffessional/1/Professional1Uk.mdx";
+import Professional1En from "./content/proffessional/1/Professional1Uk.mdx";
 
-import Content02en from "./content/02/Content02en";
-import Content02uk from "./content/02/Content02uk.mdx";
+import Professional2Uk from "./content/proffessional/2/Professional2Uk.mdx";
+import Professional2En from "./content/proffessional/2/Professional2Uk.mdx";
 
-import Content03en from "./content/03/Content03en";
-import Content03uk from "./content/03/Content03uk.mdx";
+import Professional3Uk from "./content/proffessional/3/Professional3Uk.mdx";
+import Professional3En from "./content/proffessional/3/Professional3Uk.mdx";
 
-import Content10en from "./content/10/Content10en";
-import Content10uk from "./content/10/Content10uk";
+import Professional4Uk from "./content/proffessional/4/Professional4Uk.mdx";
+import Professional4En from "./content/proffessional/4/Professional4Uk.mdx";
 
-import Content11en from "./content/11/Content11en";
-import Content11uk from "./content/11/Content11uk";
+import MoreProfessional1En from "./content/more-professional/1/MoreProfessional1Uk.mdx";
+import MoreProfessional1Uk from "./content/more-professional/1/MoreProfessional1Uk.mdx";
 
-import Content12en from "./content/12/Content12en";
-import Content12uk from "./content/12/Content12uk";
+import MoreProfessional2En from "./content/more-professional/2/MoreProfessional2Uk.mdx";
+import MoreProfessional2Uk from "./content/more-professional/2/MoreProfessional2Uk.mdx";
 
-import Content13en from "./content/13/Content13en";
-import Content13uk from "./content/13/Content13uk";
+import MoreProfessional3En from "./content/more-professional/3/MoreProfessional3Uk.mdx";
+import MoreProfessional3Uk from "./content/more-professional/3/MoreProfessional3Uk.mdx";
 
-import Content20en from "./content/20/Content20en";
-import Content20uk from "./content/20/Content20uk";
+import MoreProfessional4En from "./content/more-professional/4/MoreProfessional4Uk.mdx";
+import MoreProfessional4Uk from "./content/more-professional/4/MoreProfessional4Uk.mdx";
 
-import Content21en from "./content/21/Content21en";
-import Content21uk from "./content/21/Content21uk";
+import MorePersonal1En from "./content/more-personal/1/MorePersonal1Uk.mdx";
+import MorePersonal1Uk from "./content/more-personal/1/MorePersonal1Uk.mdx";
 
-import Content22en from "./content/22/Content22en";
-import Content22uk from "./content/22/Content22uk";
+import MorePersonal2En from "./content/more-personal/2/MorePersonal2Uk.mdx";
+import MorePersonal2Uk from "./content/more-personal/2/MorePersonal2Uk.mdx";
 
-import Content23en from "./content/23/Content23en";
-import Content23uk from "./content/23/Content23uk";
+import MorePersonal3En from "./content/more-personal/3/MorePersonal3Uk.mdx";
+import MorePersonal3Uk from "./content/more-personal/3/MorePersonal3Uk.mdx";
 
-import Content30en from "./content/30/Content30en";
-import Content30uk from "./content/30/Content30uk.mdx";
+import MorePersonal4En from "./content/more-personal/4/MorePersonal4Uk.mdx";
+import MorePersonal4Uk from "./content/more-personal/4/MorePersonal4Uk.mdx";
 
-import Content31en from "./content/31/Content31en";
-import Content31uk from "./content/31/Content31uk.mdx";
+import Personal1En from "./content/personal/1/Personal1Uk.mdx";
+import Personal1Uk from "./content/personal/1/Personal1Uk.mdx";
 
-import Content32en from "./content/32/Content32en";
-import Content32uk from "./content/32/Content32uk.mdx";
+import Personal2En from "./content/personal/2/Personal2Uk.mdx";
+import Personal2Uk from "./content/personal/2/Personal2Uk.mdx";
 
-import Content33en from "./content/33/Content33en.mdx";
-import Content33uk from "./content/33/Content33uk.mdx";
+import Personal3En from "./content/personal/3/Personal3Uk.mdx";
+import Personal3Uk from "./content/personal/3/Personal3Uk.mdx";
+
+import Personal4En from "./content/personal/4/Personal4Uk.mdx";
+import Personal4Uk from "./content/personal/4/Personal4Uk.mdx";
 
 import CvButtonSection from "./CvButtonSection";
 
 const contentMatrix: ContentMatrix = [
   [
-    { en: <Content00en key={"00"} />, uk: <Content00uk key={"00"} /> },
-    { en: <Content01en key={"01"} />, uk: <Content01uk key={"01"} /> },
-    { en: <Content02en key={"02"} />, uk: <Content02uk key={"02"} /> },
-    { en: <Content03en key={"03"} />, uk: <Content03uk key={"03"} /> },
+    { en: <Professional1En key={"00"} />, uk: <Professional1Uk key={"00"} /> },
+    { en: <Professional2En key={"01"} />, uk: <Professional2Uk key={"01"} /> },
+    { en: <Professional3En key={"02"} />, uk: <Professional3Uk key={"02"} /> },
+    { en: <Professional4En key={"03"} />, uk: <Professional4Uk key={"03"} /> },
   ],
   [
-    { en: <Content10en key={"10"} />, uk: <Content10uk key={"10"} /> },
-    { en: <Content11en key={"11"} />, uk: <Content11uk key={"11"} /> },
-    { en: <Content12en key={"12"} />, uk: <Content12uk key={"12"} /> },
-    { en: <Content13en key={"13"} />, uk: <Content13uk key={"13"} /> },
+    {
+      en: <MoreProfessional1En key={"10"} />,
+      uk: <MoreProfessional1Uk key={"10"} />,
+    },
+    {
+      en: <MoreProfessional2En key={"11"} />,
+      uk: <MoreProfessional2Uk key={"11"} />,
+    },
+    {
+      en: <MoreProfessional3En key={"12"} />,
+      uk: <MoreProfessional3Uk key={"12"} />,
+    },
+    {
+      en: <MoreProfessional4En key={"13"} />,
+      uk: <MoreProfessional4Uk key={"13"} />,
+    },
   ],
   [
-    { en: <Content20en key={"20"} />, uk: <Content20uk key={"20"} /> },
-    { en: <Content21en key={"21"} />, uk: <Content21uk key={"21"} /> },
-    { en: <Content22en key={"22"} />, uk: <Content22uk key={"22"} /> },
-    { en: <Content23en key={"23"} />, uk: <Content23uk key={"23"} /> },
+    { en: <MorePersonal1En key={"20"} />, uk: <MorePersonal1Uk key={"20"} /> },
+    { en: <MorePersonal2En key={"21"} />, uk: <MorePersonal2Uk key={"21"} /> },
+    { en: <MorePersonal3En key={"22"} />, uk: <MorePersonal3Uk key={"22"} /> },
+    { en: <MorePersonal4En key={"23"} />, uk: <MorePersonal4Uk key={"23"} /> },
   ],
   [
-    { en: <Content30en key={"30"} />, uk: <Content30uk key={"30"} /> },
-    { en: <Content31en key={"31"} />, uk: <Content31uk key={"31"} /> },
-    { en: <Content32en key={"32"} />, uk: <Content32uk key={"32"} /> },
-    { en: <Content33en key={"33"} />, uk: <Content33uk key={"33"} /> },
+    { en: <Personal1En key={"30"} />, uk: <Personal1Uk key={"30"} /> },
+    { en: <Personal2En key={"31"} />, uk: <Personal2Uk key={"31"} /> },
+    { en: <Personal3En key={"32"} />, uk: <Personal3Uk key={"32"} /> },
+    { en: <Personal4En key={"33"} />, uk: <Personal4Uk key={"33"} /> },
   ],
 ];
 
@@ -97,7 +110,7 @@ const ContentPageBody: React.FC<ContentPageBodyProps> = ({
     <motion.div
       className="relative"
       initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
+      animate={{ opacity: 1 }}
     >
       <AnimatePresence>
         <motion.div
