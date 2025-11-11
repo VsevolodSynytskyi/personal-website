6226091251ab5f3cebe23ecd241601d7c3dd750f
seva <vsevolod.synytskyi@gmail.com>
2025-11-11 02:11:32 +0200

    next intl v4 update fix

--- lib/i18n/routing.ts
+++ lib/i18n/routing.ts
@@ -1,4 +1,4 @@
-import { createSharedPathnamesNavigation } from "next-intl/navigation";
+import { createNavigation } from "next-intl/navigation";
 import { defineRouting } from "next-intl/routing";
 import { defaultLocale, locales } from "./locales";
 
@@ -12,4 +12,5 @@ export const routing = defineRouting({
 
 // Lightweight wrappers around Next.js' navigation APIs
 // that will consider the routing configuration
-createSharedPathnamesNavigation(routing);
+export const { Link, redirect, usePathname, useRouter } =
+  createNavigation(routing);
